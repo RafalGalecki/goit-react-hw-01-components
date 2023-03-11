2e9e81f19e56427ccf1e1626ad8560537cdaffa3
Rafał Gałecki <darthunter@gmail.com>
2023-03-11 17:42:01 +0100

    Statistics component added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import user from './data/user.json';
+import data from './data/data.json';
 import Profile from './components/Profile/Profile';
+import Statistics from './components/Statistics/Statistics';
 
 function App() {
   return (
     <>
       <Profile {...user} />
+      <Statistics label="Upload Stats" stats={data} />
     </>
   );
 }
---
+++ src/components/Statistics/Statistics.jsx
@@ -0,0 +1,46 @@
+import css from './Statistics.module.css';
+import PropTypes from 'prop-types';
+
+const Statistics = ({ label, stats = [] }) => {
+  return (
+    <section className={css.statistics}>
+      {stats.length > 0 ? (
+        <>
+          {label && <h2 className={css.title}>{label}</h2>}
+          <ul className={css.statList}>
+            {stats.map(el => (
+              <li
+                key={el.id}
+                className={css.item}
+                style={{ backgroundColor: getRandomHexColor() }}
+              >
+                <span className={css.label}>{el.label}</span>
+                <span className={css.percentage}>{el.percentage}%</span>
+              </li>
+            ))}
+          </ul>
+        </>
+      ) : (
+        <p className={css.paragraph}>No stats</p>
+      )}
+    </section>
+  );
+};
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+
+Statistics.propTypes = {
+  title: PropTypes.string,
+  stats: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      label: PropTypes.string.isRequired,
+      percentage: PropTypes.number.isRequired,
+    })
+  ).isRequired,
+};
+
+export default Statistics;
