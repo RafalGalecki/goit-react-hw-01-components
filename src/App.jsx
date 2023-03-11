613b965401828881470b497eb46e8250fbd942de
Rafał Gałecki <darthunter@gmail.com>
2023-03-11 20:34:17 +0100

    TransactionHistory

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import user from './data/user.json';
 import data from './data/data.json';
 import friends from './data/friends.json';
+import transactions from './data/transactions.json';
 import Profile from './components/Profile/Profile';
 import Statistics from './components/Statistics/Statistics';
 import FriendList from './components/FriendList/FriendList';
+import TransactionHistory from 'components/TransactionHistory/TransactionHistory';
+
 
 function App() {
   return (
@@ -11,6 +14,7 @@ function App() {
       <Profile {...user} />
       <Statistics label="Upload Stats" stats={data} />
       <FriendList friends={friends} />
+      <TransactionHistory transactions={transactions} />
     </>
   );
 }
---
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -0,0 +1,41 @@
+import css from './TransactionHistory.module.css';
+import PropTypes from 'prop-types';
+
+const TransactionHistory = ({ transactions = [] }) => {
+  return transactions.length > 0 ? (
+    <table className={css.transactionHistory}>
+      <thead>
+        <tr>
+          <th>Type</th>
+          <th>Amount</th>
+          <th>Currency</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        {transactions.map(transaction => (
+          <tr key={transaction.id}>
+            <td>{transaction.type}</td>
+            <td>{transaction.amount}</td>
+            <td>{transaction.currency}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  ) : (
+    <p className={css.paragraph}>No data</p>
+  );
+};
+
+TransactionHistory.propTypes = {
+  transactions: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      type: PropTypes.string.isRequired,
+      amount: PropTypes.string.isRequired,
+      currency: PropTypes.string.isRequired,
+    })
+  ).isRequired,
+};
+
+export default TransactionHistory;
