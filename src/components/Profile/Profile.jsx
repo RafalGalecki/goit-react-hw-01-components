ae9412f87a7cef07cb993dcc5a2379c2ed55b3d4
Rafał Gałecki <darthunter@gmail.com>
2023-03-11 16:11:04 +0100

    styles added

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -10,23 +10,23 @@ const Profile = ({
 }) => {
   return (
     <div className={css.profile}>
-      <div className={css.desc}>
+      <div className={css.description}>
         <img className={css.avatar} src={avatar} alt="User's avatar" />
         <p className={css.name}>{username}</p>
-        <p className={css.tag}>{tag}</p>
+        <p className={css.tag}>@{tag}</p>
         <p className={css.location}>{location}</p>
       </div>
       <ul className={css.stats}>
         <li>
-          <span className={css.label}>Followers: </span>
+          <span className={css.label}>Followers</span>
           <span className={css.quantity}>{stats.followers}</span>
         </li>
         <li>
-          <span className={css.label}>Views: </span>
+          <span className={css.label}>Views</span>
           <span className={css.quantity}>{stats.views}</span>
         </li>
         <li>
-          <span className={css.label}>Likes: </span>
+          <span className={css.label}>Likes</span>
           <span className={css.quantity}>{stats.likes}</span>
         </li>
       </ul>
